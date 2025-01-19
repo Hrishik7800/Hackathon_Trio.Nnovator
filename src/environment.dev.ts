3a88ee9e59a5988bea8fb323bab775b17a3eb7e2
Rushikesh KOLGATRAM <rushikeshk@steepgraph.com>
2025-01-20 00:22:16 +0530

    Initial commit

---
+++ src/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  
+  apiUrl: 'http://localhost:8083/3DX-ETL/',
+  apiURLARAS: 'http://localhost/InnovatorServerRelease31/',
+  apiOdataURLARAS: 'http://localhost/InnovatorServerRelease31/server/odata/'
+
+};
